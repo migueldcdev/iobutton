2e2231e39c3a59c42b7823727d17b4e870725c64
miguel <migueldc.dev@protonmail.com>
2024-12-11 15:30:14 +0100

    add: button component

--- button.tsx
+++ button.tsx
@@ -1,13 +1,52 @@
 import React from "react";
+import "./styles.css";
 
 interface ButtonProps {
-  label?: string;
+  type?: "button" | "submit" | "reset";
+  title?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  width?: string;
+  padding?: string;
+  paddingX?: string;
+  paddingY?: string;
+  background?: string;
+  color?: string;
+  border?: string;
+  borderRadius?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label = 'Click Me' }) => {
+const Button: React.FC<ButtonProps> = ({
+  title = "Click Me",
+  type = "button",
+  onClick,
+  disabled = false,
+  width = "base",
+  padding = "base",
+  background = "white",
+  color = "base",
+  border = "none",
+  borderRadius = "base",
+}) => {
+  const classNames = [
+    `w-${width}`,
+    `p-${padding}`,
+    `bg-${background}`,
+    `text-${color}`,
+    `border-${border}`,
+    `borderRadius-${borderRadius}`,
+  ].join(" ");
+
   return (
-    <button>{label}</button>
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={classNames}
+    >
+      {title}
+    </button>
   );
-}
+};
 
-export default Button
+export default Button;
